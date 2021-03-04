90b01a46e486662b1ec4bf219d79e3b8b1ffc792
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-03-04 18:10:25 +0530

    all category page changed

--- pages/category/index.js
+++ pages/category/index.js
@@ -3,41 +3,36 @@ import Link from "next/link";
 const allCategory = ({ allCategory }) => {
   return (
     <>
-<div className="container mt-5 mx-auto px-4 md:px-12 text-2xl font-bold text-gray-800">
-        All Category
-      </div>
-      <div className="container mb-12 mx-auto px-4 md:px-12">
-        <div className="flex flex-wrap -mx-1 lg:-mx-4">
-          {allCategory.map((category) => (
-            <div
-              key={category.procat_id}
-              className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/6"
+      {allCategory.map((category) => (
+        <>
+          <div
+            key={category.procat_id}
+            className="container my-3 mx-auto px-4 md:px-12 text-2xl font-bold text-gray-800"
+          >
+            <Link
+              className="no-underline hover:underline text-black"
+              href={`category/${category.slug}`}
             >
-              <article className="overflow-hidden rounded-lg">
-                <header className="flex items-center justify-between leading-tight p-2 md:p-4">
-                  <h1 className="text-lg">
-                    <Link
-                      className="no-underline hover:underline text-black"
-                      href={`category/${category.slug}`}
-                    >
-                      <a>{category.category}</a>
-                    </Link>
-                    {category.tbl_product_subcategory.map((subcategory) => (
-                      <p className="text-gray-600 text-sm leading-7">
-                        <Link href={`subcategory/${subcategory.slug}`}>
-                          <a className="hover:text-indigo-500">
-                            {subcategory.category}
-                          </a>
-                        </Link>
-                      </p>
-                    ))}
-                  </h1>
-                </header>
-              </article>
+              <a>{category.category}</a>
+            </Link>
+          </div>
+          <div className="container mb-12 mx-auto px-4 md:px-12">
+            <div className="flex flex-wrap -mx-1 lg:-mx-4">
+              {/* subcategory start */}
+              {category.tbl_product_subcategory.map((subcategory) => (
+                <div className="px-5 w-full md:w-1/2 lg:w-1/6">
+                  <Link href={`subcategory/${subcategory.slug}`}>
+                    <a className="hover:text-indigo-500">
+                      {subcategory.category}
+                    </a>
+                  </Link>
+                </div>
+              ))}
+              {/* subcategory end */}
             </div>
-          ))}
-        </div>
-      </div>
+          </div>
+        </>
+      ))}
     </>
   );
 };
