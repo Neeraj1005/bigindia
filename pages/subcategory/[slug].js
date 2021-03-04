b3d3e9df3194e833cb92e538716187375346ee7d
Neeraj1005 <neeraj.tangariya123@gmail.com>
2021-03-04 18:45:00 +0530

    wip

--- pages/subcategory/[slug].js
+++ pages/subcategory/[slug].js
@@ -114,11 +114,11 @@ export async function getStaticProps({ params }) {
       notFound: true,
     };
   }
-  if (!categoryFilter) {
-    return {
-      notFound: true,
-    };
-  }
+  // if (!categoryFilter) {
+  //   return {
+  //     notFound: true,
+  //   };
+  // }
 
   return {
     props: {
